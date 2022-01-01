4079e1442dd1915dd6ffb5d53145976b6c64c22d
U170709023 <iremuslu03@gmail.com>
2022-01-02 02:17:46 +0300

    Created fragment and selected menu ıtems.

---
+++ app/src/main/java/tr/edu/mu/ceng/mad/reminderapp/EducationCategory.java
@@ -0,0 +1,17 @@
+package tr.edu.mu.ceng.mad.reminderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class EducationCategory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_education_remind_category);
+    }
+}
